3f2816f082ef2cc5626fbadfbc590b528f254386
DFS0507 <adeyemooluwatoyin2018@gmail.com>
2025-11-24 11:13:22 +0100

    push

--- src/App.tsx
+++ src/App.tsx
@@ -8,13 +8,13 @@ import SurveyResearch from "./pages/SurveyResearch";
 import SurveyAnalysis from "./pages/SurveyAnalysis";
 import SocialListening from "./pages/SocialListening";
 import CommunityPanel from "./pages/CommunityPanel";
-import Channels from "./pages/Channels";
-import Campaigns from "./pages/Campaigns";
-import SocialInsights from "./pages/SocialInsights";
-import Report from "./pages/Report";
-import Contacts from "./pages/Contacts";
-import AudienceInsights from "./pages/AudienceInsights";
-import Settings from "./pages/Settings";
+// import Channels from "./pages/Channelss";
+// import Campaigns from "./pages/Campaignss";
+// import SocialInsights from "./pages/SocialInsightss";
+// import Report from "./pages/Reports";
+// import Contacts from "./pages/Contactss";
+// import AudienceInsights from "./pages/AudienceInsightss";
+// import Settings from "./pages/Settingss";
 import NotFound from "./pages/NotFound";
 
 const queryClient = new QueryClient();
@@ -31,13 +31,13 @@ const App = () => (
           <Route path="/survey-analysis" element={<SurveyAnalysis />} />
           <Route path="/social-listening" element={<SocialListening />} />
           <Route path="/community-panel" element={<CommunityPanel />} />
-          <Route path="/channels" element={<Channels />} />
+          {/* <Route path="/channels" element={<Channels />} />
           <Route path="/campaigns" element={<Campaigns />} />
           <Route path="/social-insights" element={<SocialInsights />} />
           <Route path="/report" element={<Report />} />
           <Route path="/contacts" element={<Contacts />} />
           <Route path="/audience-insights" element={<AudienceInsights />} />
-          <Route path="/settings" element={<Settings />} />
+          <Route path="/settings" element={<Settings />} /> */}
           {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
           <Route path="*" element={<NotFound />} />
         </Routes>
